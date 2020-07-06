3fe8adc0f963a9f7f279cf410ec67c9e489e28da
Emil VATAI <emil.vatai@gmail.com>
2020-07-06 14:45:26 +0900

    Fixup test_ssd.py (which might be a duplicate)

--- test/pytorch/test_ssd.py
+++ test/pytorch/test_ssd.py
@@ -1,7 +1,7 @@
 import logging
 import unittest
 
-from ..helpers import run_module
+from benchmarker.benchmarker import run
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -9,13 +9,13 @@ logging.basicConfig(level=logging.DEBUG)
 class PytorchVgg16Test(unittest.TestCase):
     def setUp(self):
         self.args = [
-            "benchmarker",
             "--framework=pytorch",
             "--problem=ssd",
             "--problem_size=4",
             "--batch_size=2",
+            "--nb_epoch=1",
             "--mode=inference",
         ]
 
     def test_ssd(self):
-        run_module(*self.args)
+        run(self.args)
